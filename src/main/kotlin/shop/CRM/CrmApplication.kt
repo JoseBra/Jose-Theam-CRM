5661a47fc8d687f5a244364bb79200f85d3d0d0b
Jose Braulio <josebragv@gmail.com>
2020-04-15 14:40:51 +0200

    Add Springboot skeleton from Springboot initializr

---
+++ src/main/kotlin/shop/CRM/CrmApplication.kt
@@ -0,0 +1,11 @@
+package shop.CRM
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrmApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrmApplication>(*args)
+}
